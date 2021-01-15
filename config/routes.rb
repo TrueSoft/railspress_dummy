c4c28e34384e7c5fb1009b5dd6171deeed11be91
True Soft <truesoft@ymail.com>
2021-01-15 16:30:49 +0200

    refs https://github.com/TrueSoft/railspress/issues/20

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
 
+  # Necessary for the pagination in will_paginate
+  scope path: Railspress.posts_permalink_prefix do
+    get 'page/:page' => 'railspress/home#posts'
+    get 'tag/:slug/page/:page' => 'railspress/archive#taxonomy', defaults: {taxonomy: 'post_tag'}
+    get 'author/:slug/page/:page' => 'railspress/archive#author', defaults: {taxonomy: 'author'}, as: :authors_posts
+    get ':taxonomy/:slug/page/:page' => 'railspress/archive#taxonomy'
+  end
+
   scope :admin do
     resources :options, controller: 'railspress/options' , as: :admin_options
   end
