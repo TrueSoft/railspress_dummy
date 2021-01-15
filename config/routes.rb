23702ad6195c3b8c220b5e147c3addf09894b10c
True Soft <truesoft@ymail.com>
2021-01-15 16:17:09 +0200

    Configuration for https://github.com/TrueSoft/railspress/issues/19

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,21 @@
 Rails.application.routes.draw do
 
-  get 'news' => 'railspress/posts#index', as: :all_posts
-  get 'news/:year' => 'railspress/posts#by_year', constraints: {year: /20[12]\d/}, as: :news_of_year
-  get 'news/:year/:month' => 'railspress/posts#by_month', constraints: {year: /20[12]\d/, month: /(0?\d)|10|11|12/}
-  scope :news do
-    get ':slug' => 'railspress/posts#single'
-    get 'tag/:slug' => 'railspress/posts#archive', defaults: {taxonomy: 'post_tag'}
-    get ':taxonomy/:slug' => 'railspress/posts#archive'
-  end
-
-  get 'page/*slug' => 'railspress/pages#show', as: :show_page
-
   scope :admin do
     resources :options, controller: 'railspress/options' , as: :admin_options
   end
 
-  get 'test' => 'railspress/pages#test'
+    get 'test' => 'railspress/home#testing_page'
 
-  # mount Railspress::Engine => "/railspress"
+  # get '*slug' => 'railspress/posts#single', as: :show_page
 
-  root 'railspress/pages#index'
+  mount Railspress::Engine => '/'
+
+  # scope :news do
+  #   # get ':slug' => 'railspress/posts#single'
+  #   get 'tag/:slug' => 'railspress/archive#taxonomy', defaults: {taxonomy: 'post_tag'}
+  #   get ':taxonomy/:slug' => 'railspress/archive#taxonomy'
+  # end
+
+  root 'railspress/home#index'
 
 end
